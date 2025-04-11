822cb908eb53ccf7ff1e2a1ecc862da25516e4c1
avinashsumanmakka <avi.bak46@gmail.com>
2025-04-11 07:37:33 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vnet_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "address_space" {
+  type = list(string)
+}
+variable "subnets" {
+  type = map(object({
+    address_prefixes = list(string)
+  }))
+}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+variable "create_nsg" {
+  type    = bool
+  default = false
+}
